91546e0b69431b082a308c01f5e48496bd288e67
jldemiguel <jldmp2001@gmail.com>
2025-01-02 09:38:45 +0100

    Added test endpoint

---
+++ src/test/java/com/whatstheplan/notifications/integration/NotificationsControllerIntegrationTest.java
@@ -0,0 +1,21 @@
+package com.whatstheplan.notifications.integration;
+
+import com.whatstheplan.notifications.testconfig.BaseIntegrationTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+class NotificationsControllerIntegrationTest extends BaseIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testEndpoint_ShouldReturnUp() throws Exception {
+        mockMvc.perform(get("/notifications/test"))
+                .andExpect(status().isOk());
+    }
+}
