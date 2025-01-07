faf072d6d4a70d60148a19ee571f71f3a5a22662
jldemiguel <jldmp2001@gmail.com>
2025-01-07 18:54:49 +0100

    Added OAuth security and RBAC

--- src/test/java/com/whatstheplan/notifications/NotificationsApplicationTests.java
+++ src/test/java/com/whatstheplan/notifications/NotificationsApplicationTests.java
@@ -1,13 +1,14 @@
 package com.whatstheplan.notifications;
 
+import com.whatstheplan.notifications.testconfig.BaseIntegrationTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class NotificationsApplicationTests {
+class NotificationsApplicationTests extends BaseIntegrationTest {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
